612b17959e0896884b4c4d2487ecb26068881b82
RiohRowe <55951352+RiohRowe@users.noreply.github.com>
2020-02-17 09:19:56 -0500

    Rioh, Added New Spring Starter Project (Empty)

---
+++ CustomerQueryTracker/src/main/java/com/rioh/CustomerQueryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rioh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerQueryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerQueryTrackerApplication.class, args);
+	}
+
+}
---
+++ CustomerQueryTracker/src/test/java/com/rioh/CustomerQueryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rioh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerQueryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
